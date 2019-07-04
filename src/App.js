7750d0f8dc0201569208818561962ca743bee914
Vitor Freitas <vitorldfreitas@gmail.com>
2019-07-03 21:31:12 -0300

    feature: save user's token on redux on first loading

--- src/App.js
+++ src/App.js
@@ -3,31 +3,45 @@ import { HashRouter, Switch, Route, Redirect } from 'react-router-dom';
 import { CssBaseline } from '@material-ui/core';
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 import MomentUtils from '@date-io/moment';
-import { Provider } from 'react-redux';
+import { connect } from 'react-redux';
 import routes from './routes';
-import configureStore from './redux/store';
 import 'moment/locale/pt-br';
+import { setUserToken } from './redux/actions/user';
 
-function App() {
+function App({ setUserToken }) {
+  const [reduxTokenUpdated, setUpdateState] = React.useState(false);
   const token = localStorage.getItem('token');
 
-  return (
-    <Provider store={configureStore()}>
-      <MuiPickersUtilsProvider utils={MomentUtils}>
-        <HashRouter>
-          <CssBaseline />
-          <Switch>
-            {routes.map(route => (
-              <Route key={route.path} {...route} />
-            ))}
+  // Update token on redux preventing the view to update
+  // multiple times, which causes a maximum call stack error.
+  if (!reduxTokenUpdated && token) {
+    setUserToken(token);
+    setUpdateState(true);
+  }
 
-            {/* TODO: Save token on Redux */}
-            <Redirect from="/" to={token ? '/dashboard' : '/login'} />
-          </Switch>
-        </HashRouter>
-      </MuiPickersUtilsProvider>
-    </Provider>
+  return (
+    <MuiPickersUtilsProvider utils={MomentUtils}>
+      <HashRouter>
+        <CssBaseline />
+        <Switch>
+          {routes.map(route => (
+            <Route key={route.path} {...route} />
+          ))}
+
+          <Redirect from="/" to={token ? '/dashboard' : '/login'} />
+        </Switch>
+      </HashRouter>
+    </MuiPickersUtilsProvider>
   );
 }
 
-export default App;
+const mapStateToProps = state => ({ ...state });
+
+const mapDispatchToProps = {
+  setUserToken
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
